64b1247ac93ee9a399fadaf1de0d2e8374206a86
pavitgopal <pavitgopal@gmail.com>
2024-09-11 11:20:57 -0500

    added to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,16 @@ more_movies = [
   { title: 'Nomadland', rating: 'R',
     release_date: '19-Feb-2021' },
   { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' }
+    release_date: '13-Aug-2021' },
+  { title: 'Inception', rating: 'PG-13',
+    release_date: '16-Jul-2010' },
+  { title: 'The Matrix', rating: 'R',
+    release_date: '31-Mar-1999' },
+  { title: 'The Godfather', rating: 'R',
+    release_date: '24-Mar-1972' }
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  # Use find_or_create_by to prevent duplicates
+  Movie.find_or_create_by(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
 end
